fc273303454adf8c16e3255beed1fda3ad9e779d
Manoj Kumar Narayanasetty <manojkumar@Manojs-MacBook-Air.local>
2025-04-11 16:20:18 +0530

    Interview updates

--- GymMemberShipApi.js
+++ GymMemberShipApi.js
@@ -7,17 +7,46 @@ app.use(express.json());
 
 
 let members = []; // list to store gym members
-
+let slots = [5,5,0,0]; // 0 - mrng,1-noon,2-evng,3-night
 // Register a new gym membership
 app.post('/register', (req, res) => {
-    const { name, email, startDate } = req.body;
+    const { name, email, startDate,slot } = req.body;
     if (members.some(member => member.email === email)) {
         return res.status(400).json({ message: 'Member already exists' });
     }
-    
-    const newMember = { name, email, startDate };
+    const slotAvailable = false;
+    const availableSlots = [];
+        if(slots[slot]>0){
+            slotAvailable = true;
+            slots[slot]--;
+        } else {
+            if(slots[0]>0){
+                availableSlots.push('Morning');
+               // return res.status(400).json({ message: 'Morning slot available' });
+            }
+            if(slots[1]>0){
+                availableSlots.push('Afternoon');
+               // return res.status(400).json({ message: 'Afternoon slot available' });
+            } if(slots[2]>0){
+                availableSlots.push('Evening');
+                // return res.status(400).json({ message: 'Evng slot available' });
+            }if (slots[3]>0){
+                availableSlots.push('Night');
+               //  return res.status(400).json({ message: 'Night slot available' });
+            }
+            if(availableSlots.length > 0){
+                return res.status(400).json({message:'These are the available slots:', availableSlots});
+            }
+            else {
+                return res.status(400).json({ message: 'No slots available'});
+            }
+        }
+    if(slotAvailable){
+    const newMember = { name, email, startDate};
     members.push(newMember);
     res.status(201).json({ message: 'Membership registered successfully', member: newMember });
+    }
+
 });
 
 // View membership details
